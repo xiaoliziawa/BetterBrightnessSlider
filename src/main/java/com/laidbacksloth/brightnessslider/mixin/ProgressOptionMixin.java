89bfd41f84e6f21b5a7330cc8e94c8eaeb9691f9
LaidBackSloth42 <109034679+LaidBackSloth@users.noreply.github.com>
2022-10-10 18:07:20 +0200

    backported to 1.16.5

--- src/main/java/com/laidbacksloth/brightnessslider/mixin/ProgressOptionMixin.java
+++ src/main/java/com/laidbacksloth/brightnessslider/mixin/ProgressOptionMixin.java
@@ -1,13 +1,11 @@
 package com.laidbacksloth.brightnessslider.mixin;
 
 import com.laidbacksloth.brightnessslider.BetterBrightnessSlider;
+import net.minecraft.client.GameSettings;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.Options;
-import net.minecraft.client.ProgressOption;
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TextComponent;
-import net.minecraft.network.chat.TranslatableComponent;
-import net.minecraft.util.FormattedCharSequence;
+import net.minecraft.client.settings.SliderPercentageOption;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -16,12 +14,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
-@Mixin(ProgressOption.class)
+@Mixin(SliderPercentageOption.class)
 public class ProgressOptionMixin {
     @Shadow
     @Final
@@ -35,15 +32,15 @@ public class ProgressOptionMixin {
     @Shadow
     @Final
     @Mutable
-    private BiConsumer<Options, Double> setter;
+    private BiConsumer<GameSettings, Double> setter;
 
     @Shadow
     @Final
     @Mutable
-    private BiFunction<Options, ProgressOption, Component> toString;
+    private BiFunction<GameSettings, SliderPercentageOption, ITextComponent> toString;
 
     @Inject(method = "<init>*", at = @At("RETURN"), remap = false)
-    protected void init(String pCaptionKey, double pMinValue, double pMaxValue, float pSteps, Function<Options, Double> pGetter, BiConsumer<Options, Double> pSetter, BiFunction<Options, ProgressOption, Component> pToString, Function<Minecraft, List<FormattedCharSequence>> pTooltipSupplier, CallbackInfo info) {
+    protected void init(String pCaptionKey, double pMinValue, double pMaxValue, float pSteps, Function<GameSettings, Double> pGetter, BiConsumer<GameSettings, Double> pSetter, BiFunction<GameSettings, SliderPercentageOption, ITextComponent> pToString, CallbackInfo info) {
         if (pCaptionKey.equals("options.gamma")) {
             this.minValue = BetterBrightnessSlider.min;
             this.maxValue = BetterBrightnessSlider.max;
@@ -52,12 +49,12 @@ public class ProgressOptionMixin {
         }
     }
 
-    private void setter(Options options, Double brightness) {
+    private void setter(GameSettings options, Double brightness) {
         brightness = Math.round(brightness / BetterBrightnessSlider.interval) * BetterBrightnessSlider.interval;
         Minecraft.getInstance().options.gamma = brightness;
     }
 
-    private Component toString(Options options, ProgressOption progressOption) {
-        return new TranslatableComponent("options.gamma").append(": ").append(new TextComponent(Math.round(options.gamma * 100) + "%"));
+    private ITextComponent toString(GameSettings options, SliderPercentageOption progressOption) {
+        return new TranslationTextComponent("options.gamma").append(": ").append(Math.round(options.gamma * 100) + "%");
     }
 }
--- src/main/java/com/laidbacksloth/brightnessslider/mixin/SliderControlMixin.java
+++ src/main/java/com/laidbacksloth/brightnessslider/mixin/SliderControlMixin.java
@@ -4,11 +4,13 @@ import com.laidbacksloth.brightnessslider.BetterBrightnessSlider;
 import me.jellysquid.mods.sodium.client.gui.options.Option;
 import me.jellysquid.mods.sodium.client.gui.options.control.ControlValueFormatter;
 import me.jellysquid.mods.sodium.client.gui.options.control.SliderControl;
-import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Objects;
 
 @Pseudo
 @Mixin(SliderControl.class)
@@ -30,7 +32,7 @@ public class SliderControlMixin {
 
     @Inject(method = "<init>", at = @At("RETURN"), remap = false)
     public void init(Option<Integer> option, int min, int max, int interval, ControlValueFormatter mode, CallbackInfo info) {
-        if (option.getName() instanceof TranslatableComponent component && component.getKey().equals("options.gamma")) {
+        if (Objects.equals(option.getName(), new TranslationTextComponent("options.gamma").getString())) {
             this.min = (int) (BetterBrightnessSlider.min * 100);
             this.max = (int) (BetterBrightnessSlider.max * 100);
             this.interval = (int) (BetterBrightnessSlider.interval * 100);
