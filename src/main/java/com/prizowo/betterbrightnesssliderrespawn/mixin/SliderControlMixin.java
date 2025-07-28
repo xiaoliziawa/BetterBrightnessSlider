c3c4fb42d9a6c8f14682f6a97809a5efd3fc2594
xiaoliziawa <3682822069@qq.com>
2025-07-28 23:24:35 +0800

    change emb to sodium

--- src/main/java/com/prizowo/betterbrightnesssliderrespawn/mixin/SliderControlMixin.java
+++ src/main/java/com/prizowo/betterbrightnesssliderrespawn/mixin/SliderControlMixin.java
@@ -1,10 +1,10 @@
 package com.prizowo.betterbrightnesssliderrespawn.mixin;
 
 import com.prizowo.betterbrightnesssliderrespawn.Betterbrightnesssliderrespawn;
+import net.caffeinemc.mods.sodium.client.gui.options.Option;
+import net.caffeinemc.mods.sodium.client.gui.options.control.ControlValueFormatter;
+import net.caffeinemc.mods.sodium.client.gui.options.control.SliderControl;
 import net.minecraft.network.chat.contents.TranslatableContents;
-import org.embeddedt.embeddium.api.options.control.ControlValueFormatter;
-import org.embeddedt.embeddium.api.options.control.SliderControl;
-import org.embeddedt.embeddium.api.options.structure.Option;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -29,7 +29,7 @@ public class SliderControlMixin {
     private int interval;
 
     @Inject(at = @At("RETURN"), method = "<init>", remap = false)
-    public void init(Option option, int min, int max, int interval, ControlValueFormatter mode, CallbackInfo ci) {
+    public void init(Option<Integer> option, int min, int max, int interval, ControlValueFormatter mode, CallbackInfo ci) {
         if (option.getName().getContents() instanceof TranslatableContents component && component.getKey().equals("options.gamma")) {
             this.min = (int) (Betterbrightnesssliderrespawn.min * 100);
             this.max = (int) (Betterbrightnesssliderrespawn.max * 100);
